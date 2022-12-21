4a61494d68bcc9735bebf12d0866f9b116ea0a0d
Pepitos17 <msogodogo@gmail.com>
2022-12-21 12:35:31 +0100

    feat: Add Student Class

--- src/main/java/net/joyboyrest/SpringBootRestApiApplication.java
+++ src/main/java/net/joyboyrest/SpringBootRestApiApplication.java
@@ -1,10 +1,13 @@
-package net.joyboyrest.SpringBootRestApi;
+package net.joyboyrest;
 
+import net.joyboyrest.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -17,8 +20,16 @@ public class SpringBootRestApiApplication {
 
 	}
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello","World");
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Mario",
+						"mario.jb@gmail.com",
+						LocalDate.of(2000, Month.JANUARY,5),
+						21
+				)
+		);
 
 	}
 
